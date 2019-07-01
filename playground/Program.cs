cd0b1d63e2dce63a1dc4963dace08c2d03b9788d
MacyJDraper <macyj93@att.net>
2019-07-01 10:49:22 -0500

    "Learning More About Classes"

--- playground/Program.cs
+++ playground/Program.cs
@@ -5,32 +5,47 @@ namespace playground
 {
     class Program
     {
-        public class Cat
+       static void Main()
         {
-            public string Name { get; set; }
-            public string Color { get; set; }
-            public string Age { get; set; }
+            Cat cat1 = new Cat(); // You can write like this or like line 15-25
+            cat1.Name = "Oscar";
+            cat1.Color = "brown";
+            cat1.Age = 1;
 
-            public void MeetTheCat()
-            {
-                Console.WriteLine("Hello, I'm " + Name + " the " + Color + " colored cat!");
-            }
+            Cat cat2 = new Cat();
+            cat2.Name = "Tim";
+            cat2.Color = "brown";
+            cat2.Age = 1;
+
+            Cat cat3 = new Cat();
+            cat2.Name = "Simon";
+            cat2.Age = 14;
+
+            //string catName1 = "Oscar";
+            //string catColor1 = "brown";
+            //int catAge1 = 1;
+
+            //string catName2 = "Tim";
+            //string catName2 = "orange";
+            //int catAge2 = 5;
+
+            //string catName3 = "Simon";
+            //string catName3 = "white";
+            //int catAge3 = 14;
+
+            Console.WriteLine("Cats 1's name is " + cat1.Name);
+            Console.WriteLine("Cats 2's name is " + cat2.Name);
+            Console.WriteLine("Cats 3's name is " + cat3.Name);
+        }
+        static void IntroduceCat(string name, string color, int age)
+        {
 
         }
-        static void Main(string[] args)
-        {
-            var cat = new Cat(); //same as bellow you can do it either way. 
-            cat.Name = "Oscar";
-            cat.Color = "brown";
-            cat.MeetTheCat();
-            var cat2 = new Cat()
-            {
-                Name = "Sebastian", //same as above you can do it either way.
-                Color = "Gray"
-            };
-            cat2.MeetTheCat();
-        }
-        
     }
-    
+    class Cat
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+    }
 }
