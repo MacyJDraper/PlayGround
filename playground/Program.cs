9170899f745cad5d021010080381756a3d45ef23
MacyJDraper <macyj93@att.net>
2019-07-01 11:03:07 -0500

    More on classes

--- playground/Program.cs
+++ playground/Program.cs
@@ -5,40 +5,18 @@ namespace playground
 {
     class Program
     {
-       static void Main()
+        static void Main(string[] args)
         {
             Cat cat1 = new Cat(); // You can write like this or like line 15-25
             cat1.Name = "Oscar";
             cat1.Color = "brown";
-            cat1.Age = 1;
 
-            Cat cat2 = new Cat();
-            cat2.Name = "Tim";
-            cat2.Color = "brown";
-            cat2.Age = 1;
+            Random rnd = new Random();
 
-            Cat cat3 = new Cat();
-            cat2.Name = "Simon";
-            cat2.Age = 14;
-
-            //string catName1 = "Oscar";
-            //string catColor1 = "brown";
-            //int catAge1 = 1;
-
-            //string catName2 = "Tim";
-            //string catName2 = "orange";
-            //int catAge2 = 5;
-
-            //string catName3 = "Simon";
-            //string catName3 = "white";
-            //int catAge3 = 14;
+            rnd.Next();
+            cat1.IntroduceCat();
 
             Console.WriteLine("Cats 1's name is " + cat1.Name);
-            Console.WriteLine("Cats 2's name is " + cat2.Name);
-            Console.WriteLine("Cats 3's name is " + cat3.Name);
-        }
-        static void IntroduceCat(string name, string color, int age)
-        {
 
         }
     }
@@ -46,6 +24,10 @@ namespace playground
     {
         public string Name { get; set; }
         public string Color { get; set; }
-        public int Age { get; set; }
+
+        public void IntroduceCat()
+        {
+            Console.WriteLine("Hi! I'm " + Name + "!");
+        }
     }
 }
