f813ba2f4098587ff2a309085e315ace05043c0a
MacyJDraper <macyj93@att.net>
2019-07-02 12:47:59 -0500

    Cost analysis

---
+++ playground/LemonadeStand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace playground
+{
+    public class LemonadeStand
+    {
+        public string Name { get; set; }
+        public decimal TableCost { get; set; }
+        public decimal ChairCost { get; set; }
+        public decimal SignCost { get; set; }
+        public decimal LemonsCostPerCup { get; set; }
+        public decimal SugarCostPerCup { get; set; }
+        public decimal PricePerCup { get; set; }
+        public int NumberOfCupsSold { get; set; }
+
+    }
+}
--- playground/Program.cs
+++ playground/Program.cs
@@ -1,17 +1,43 @@
-﻿namespace playground
+﻿using System;
+
+namespace playground
+
 {
     class Program
     {
+        static decimal GetDecimalAnswerFromUser(string question)
+        {
+            Console.WriteLine(question);
+            string response = Console.ReadLine();
+            decimal answer = decimal.Parse(response);
+            return answer;
+        }
+
         static void Main(string[] args)
         {
-            Dog myExampleDog = new Dog("Spot");
-            //myExampleDog.Name = "Spot"; //line is no longer needed because of line above.
-            myExampleDog.NumberOfLegs = 4;
+            LemonadeStand stand = new LemonadeStand();
 
-            Dog mySecondDog = new Dog() { Name = "Spot", NumberOfLegs = 4 };
+            Console.WriteLine("What is the name of this lemondade stand?");
+            stand.Name = Console.ReadLine();
 
-            myExampleDog.Speak();
-            mySecondDog.Speak(); //parens are metjod invocation
+            stand.TableCost = GetDecimalAnswerFromUser("How much will your table cost?");
+            stand.ChairCost = GetDecimalAnswerFromUser("How much will your chair cost?");
+            stand.SignCost = GetDecimalAnswerFromUser("How much will your sign cost?");
+            stand.LemonsCostPerCup = GetDecimalAnswerFromUser("How much will the lemons cost per cup?");
+            stand.SugarCostPerCup = GetDecimalAnswerFromUser("How much will the sugar cost per cup?");
+            stand.PricePerCup = GetDecimalAnswerFromUser("What will be the price of a cup?");
+
+            Console.WriteLine("How many cups will you sell?");
+            string numberOfCupsResponse = Console.ReadLine();
+            stand.NumberOfCupsSold = int.Parse(numberOfCupsResponse);
+
+            decimal totalFixedCosts = stand.TableCost + stand.ChairCost + stand.SignCost;
+            decimal totalVariableCost = (stand.LemonsCostPerCup + stand.SugarCostPerCup) * stand.NumberOfCupsSold;
+            decimal totalRevenue = stand.PricePerCup * stand.NumberOfCupsSold;
+
+            decimal totalProfit = totalRevenue - totalVariableCost - totalFixedCosts;
+
+            Console.WriteLine("Your profit will be $" + totalProfit);
         }
       
     }
