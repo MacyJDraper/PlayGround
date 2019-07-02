0b8f282ff442a9112e9759e30f9cf0d6e919fb29
MacyJDraper <macyj93@att.net>
2019-07-02 10:37:01 -0500

    making them give string to run

--- playground/Dog.cs
+++ playground/Dog.cs
@@ -5,6 +5,11 @@ namespace playground
 {
     class Dog
     {
+        public Dog(string dogName) //constructor method: const tab tab;  this method will not have a return type. The name of method will always be the name of the class
+        {
+            Name = dogName;
+            Console.WriteLine("Instantiating a dog..");
+        }
         public string Name { get; set; }
         public int NumberOfLegs { get; set; }
 
--- playground/Program.cs
+++ playground/Program.cs
@@ -4,11 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Dog myExampleDog = new Dog();
-            myExampleDog.Name = "Spot";
+            Dog myExampleDog = new Dog("Spot");
+            //myExampleDog.Name = "Spot"; //line is no longer needed because of line above.
             myExampleDog.NumberOfLegs = 4;
 
             Dog mySecondDog = new Dog() { Name = "Spot", NumberOfLegs = 4 };
+
+            myExampleDog.Speak();
+            mySecondDog.Speak(); //parens are metjod invocation
         }
       
     }
